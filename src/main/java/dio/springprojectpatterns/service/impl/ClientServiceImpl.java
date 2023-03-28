6e2d5f0d89a057ddf1f90699e9f815aa076703a5
leofab <leofabr3@gmail.com>
2023-03-28 17:31:11 -0300

    ClientServiceImpl class implement

---
+++ src/main/java/dio/springprojectpatterns/service/impl/ClientServiceImpl.java
@@ -0,0 +1,31 @@
+package dio.springprojectpatterns.service.impl;
+
+import dio.springprojectpatterns.model.Client;
+import dio.springprojectpatterns.service.ClientService;
+
+public class ClientServiceImpl  implements ClientService {
+    @Override
+    public Iterable<Client> findAll() {
+        return null;
+    }
+
+    @Override
+    public Client findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void insert(Client client) {
+
+    }
+
+    @Override
+    public void update(Long id, Client client) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
