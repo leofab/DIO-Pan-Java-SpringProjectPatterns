764e208944d76b4b127f9172a96246c3f41de807
leofab <leofabr3@gmail.com>
2023-03-28 17:29:15 -0300

    ClientService interface implement

--- src/main/java/dio/springprojectpatterns/repository/AdressRepo.java
+++ src/main/java/dio/springprojectpatterns/repository/AdressRepo.java
@@ -2,6 +2,8 @@ package dio.springprojectpatterns.repository;
 
 import dio.springprojectpatterns.model.Adress;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdressRepo extends CrudRepository<Adress, String > {
 }
---
+++ src/main/java/dio/springprojectpatterns/service/ClientService.java
@@ -0,0 +1,16 @@
+package dio.springprojectpatterns.service;
+
+import dio.springprojectpatterns.model.Client;
+
+public interface ClientService {
+
+    Iterable<Client> findAll();
+
+    Client findById(Long id);
+
+    void insert(Client client);
+
+    void update(Long id, Client client);
+
+    void delete(Long id);
+}
