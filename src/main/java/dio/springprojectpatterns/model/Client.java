fb31d92577e53827ed5421d6146a0b39f2748357
leofab <leofabr3@gmail.com>
2023-03-28 17:57:24 -0300

    Adress & Client class implement

--- src/main/java/dio/springprojectpatterns/model/Client.java
+++ src/main/java/dio/springprojectpatterns/model/Client.java
@@ -1,4 +1,22 @@
 package dio.springprojectpatterns.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @ManyToOne
+    private Adress adress;
+
+
 }
---
+++ src/main/java/dio/springprojectpatterns/service/ViaCepService.java
@@ -0,0 +1,12 @@
+package dio.springprojectpatterns.service;
+
+import dio.springprojectpatterns.model.Adress;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+    @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
+    Adress 
+}
