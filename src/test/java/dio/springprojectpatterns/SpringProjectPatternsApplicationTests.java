a8339b947f3dab23fe895b649703d99dc7546436
leofab <leofabr3@gmail.com>
2023-03-28 16:32:00 -0300

    first commit

---
+++ src/test/java/dio/springprojectpatterns/SpringProjectPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springprojectpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
