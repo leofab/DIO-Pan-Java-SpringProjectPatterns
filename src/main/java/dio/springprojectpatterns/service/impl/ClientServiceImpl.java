d26685d4a97f06ca885b644bfc36822e70f82e25
leofab <leofabr3@gmail.com>
2023-03-29 14:57:48 -0300

    annotation @Component in ClientServiceImpl that was causing this.ClientService to point to null

--- src/main/java/dio/springprojectpatterns/service/impl/ClientServiceImpl.java
+++ src/main/java/dio/springprojectpatterns/service/impl/ClientServiceImpl.java
@@ -7,10 +7,11 @@ import dio.springprojectpatterns.repository.ClientRepo;
 import dio.springprojectpatterns.service.ClientService;
 import dio.springprojectpatterns.service.ViaCepService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-
+@Component
 public class ClientServiceImpl implements ClientService {
 
     @Autowired
